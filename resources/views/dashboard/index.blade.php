815675ba559cf52690a7cf005316249bfe220a0d
fourki <fourki2002@gmail.com>
2023-07-08 17:48:55 +0700

    avatar succes

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -40,7 +40,7 @@
                     <li class="nav-item nav-profile dropdown">
                         <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown"
                             id="profileDropdown">
-                            <img src="{{ asset('admin') }}/images/faces/face5.jpg" alt="profile" />
+                            <img src="{{ asset('admin') }}/images/faces/{{ Auth::user()->avatar }}" alt="profile" />
                             <span class="nav-profile-name">{{ Auth::user()->name }}</span>
                         </a>
                         <div class="dropdown-menu dropdown-menu-right navbar-dropdown"
