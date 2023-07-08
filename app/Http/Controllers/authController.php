ea9f290ce03d0192df46638aaf802d37c1e7ad90
fourki <fourki2002@gmail.com>
2023-07-08 14:52:18 +0700

    fucntion auth controller

--- app/Http/Controllers/authController.php
+++ app/Http/Controllers/authController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
 
 class authController extends Controller
 {
-    //
+    function redirect() 
+    {
+        return Socialite::driver('google')->redirect();
+    }
+    function callback()
+    {
+        $user = Socialite::driver('google')->user();
+    $id = $user->id;
+    $email = $user->email;
+    $name = $user->name;
+
+    return "$id - $email - $name";
+    }
 }
